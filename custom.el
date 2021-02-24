e9b1ed6b2ec19e4b772a4702d4ddff1b5fdf8f30
fochive <fochive00@fochive00@gmail.com>
2021-02-24 22:19:04 +0800

    custumize org-mode

--- custom.el
+++ custom.el
@@ -3,10 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (session yasnippet web-mode vue-mode use-package tide rjsx-mode python-mode org-bullets multiple-cursors magit lsp-vue lsp-ui lsp-typescript lsp-python lsp-javascript-typescript json-mode htmlize highlight-indent-guides helm-projectile geiser engine-mode dired-filter diminish company-web company-quickhelp company-lsp avy)))
- '(session-use-package t nil (session)))
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
